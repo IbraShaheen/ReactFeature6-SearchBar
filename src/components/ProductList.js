31898d1084c7bf348dec7241a608def5abd4b4d4
IbraShaheen <engibraheem1995@gmail.com>
2021-06-06 11:50:00 +0300

    effective search

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,17 +3,25 @@ import { ListWrapper } from "../styles";
 // Components
 import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
+import { useState } from "react";
+
 // Data
 import products from "../products";
 
 const ProductList = () => {
-  const productList = products.map((product) => (
+  const [query, setQuery] = useState("a");
+  const SearchArray = products.filter(product => product.name.toUpperCase().includes(query.toUpperCase()))
+    // console.log({SearchArray});
+
+  const productList = SearchArray.map((product) => (
     <ProductItem product={product} key={product.id} />
   ));
 
+
+
   return (
     <>
-      <SearchBar />
+      <SearchBar setQuery={setQuery} />
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
